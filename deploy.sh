a6b758a4a6e52936e14431828eb5806073c43134
carol liao <desire11012@gmail.com>
2021-09-19 14:27:17 +0800

    修改路徑

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/carolxuan/wasa-test.git master:gh-pages
+git push -f https://github.com/carolxuan/WasaTeam-test.git master:gh-pages
 
 cd -
