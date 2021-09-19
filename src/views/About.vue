89989d7f25298ca612957de6c6b37bf3e0f4c5db
carol liao <desire11012@gmail.com>
2021-09-19 13:51:16 +0800

    新增 scss、axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about bg-primary">
+    <div class="container text-white">
+      <h2 class="l-title">About our teams</h2>
+      <ul class="about-card wrap">
+        <li v-for="item in temp" :key="item.name">
+          <img :src="item.imageUrl" :alt="item.name">
+          <h4 class="about-card-txt">{{ item.name }}</h4>
+          <p class="about-card-txt">{{ item.city }}</p>
+          <p class="about-card-txt">{{ item.phone }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      temp: {}
+    }
+  },
+  methods: {
+    getData () {
+      const url = 'https://mocki.io/v1/497557a8-a2a1-4e5a-8b34-4025e9558793'
+      this.$http.get(url)
+        .then(res => {
+          this.temp = res.data
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+  created () {
+    this.getData()
+  }
+}
+</script>
